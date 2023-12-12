9926a8f5f32ae0cd84c2ef4b8704a087e2f6edd3
Yohan Soundara <yohan.soundara@gmail.com>
2023-12-12 18:05:33 +0100

    mew ex3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,10 +2,24 @@
 function PokemonCard() {
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-        <figcaption>Bulbasaur</figcaption>
+            {pokemonList[1].imgSrc !== undefined 
+            ? (<img src={pokemonList[1].imgSrc} />)
+            : (<p>???</p>)}
+        <figcaption>{pokemonList[1].name}</figcaption>
         </figure>
     );
 }
 
 export default PokemonCard;
+
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
